c60cd3d16949cac12d345a0fb9ade77517b61294
Anton <anton040795@gmail.com>
2022-05-26 14:57:54 +0400

    Initial commit

---
+++ src/test/java/pro/sky/course_project2/CourseProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
